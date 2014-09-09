6a1929fc52fa09309e9af7e37ce1d8726b0f96b8
Jennifer Huang <jennhuang8@gmail.com>
2014-09-08 22:17:18 -0700

    trying to get the card to show up

--- gen/com/example/ok_glass_find_me_food/R.java
+++ gen/com/example/ok_glass_find_me_food/R.java
@@ -10,6 +10,9 @@ package com.example.ok_glass_find_me_food;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f050000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,26 +21,27 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int seconds=0x7f0a0000;
+        public static final int stop=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int glass_voice_trigger=0x7f060003;
-        public static final int hello_world=0x7f060001;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int glass_voice_trigger=0x7f070003;
+        public static final int stop=0x7f070001;
     }
     public static final class style {
         /** 
@@ -61,11 +65,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int voice_trigger=0x7f040000;
