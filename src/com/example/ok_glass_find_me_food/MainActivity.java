b8ab0555d7f17c6bb26ec9d4f157d16ffe1449e3
Jennifer Huang <jennhuang8@gmail.com>
2014-09-09 22:24:48 -0700

    still trying to get blank card to show up

--- src/com/example/ok_glass_find_me_food/MainActivity.java
+++ src/com/example/ok_glass_find_me_food/MainActivity.java
@@ -1,63 +1,36 @@
 package com.example.ok_glass_find_me_food;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
-    private final Handler mHandler = new Handler();
-
     @Override
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-        openOptionsMenu();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        //inflater.inflate(R.menu.stopwatch, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection.
-        switch (item.getItemId()) {
-            case R.id.stop:
-                // Stop the service at the end of the message queue for proper options menu
-                // animation. This is only needed when starting a new Activity or stopping a Service
-                // that published a LiveCard.
-                post(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        //stopService(new Intent(MenuActivity.this, StopwatchService.class));
-                    }
-                });
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onOptionsMenuClosed(Menu menu) {
-        // Nothing else to do, closing the Activity.
-        finish();
-    }
-
-    /**
-     * Posts a {@link Runnable} at the end of the message loop, overridable for testing.
-     */
-    protected void post(Runnable runnable) {
-        mHandler.post(runnable);
-    }
-
 }
